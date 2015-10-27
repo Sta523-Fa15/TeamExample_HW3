b6f4aeac8d82a8035752fede39bc6c79a4d84bdb
Colin Rundel <rundel@gmail.com>
2015-10-26 20:10:35 -0400

    Fix name column

--- model.R
+++ model.R
@@ -18,7 +18,6 @@ names(d)  = c("Borough","long","lat")
 ## Create model
 l = multinom(Borough~long*lat+I(long^2)+I(lat^2),data=d)
 
-
 ## Create raster for prediction locations
 r = raster(nrows=500, ncols=500, 
            xmn=-74.3, xmx=-73.71, 
@@ -35,7 +34,7 @@ r[] = predict(l,pred_locs)
 
 poly = rasterToPolygons(r,dissolve=TRUE)
 
-names(poly@data) = "Names"
+names(poly@data) = "Name"
 poly@data$Names = short_to_long[levels(pred)]
 
 source("write_geojson.R")
